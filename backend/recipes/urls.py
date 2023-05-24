cc166f2bed895ec6e9bfce8c725c858d5645f262
Murat <m.kaniukov@gmail.com>
2023-05-24 18:21:06 +0200

    on server2

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,15 +1,15 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import IngredientViewSet, RecipesViewSet, TagViewSet
+from .views import IngredientsViewSet, RecipeViewSet, TagsViewSet
 
 app_name = 'recipes'
 
 api_recipes_router_v1 = DefaultRouter()
-api_recipes_router_v1.register(r'tags', TagViewSet, basename='tags')
-api_recipes_router_v1.register(r'ingredients', IngredientViewSet,
+api_recipes_router_v1.register(r'tags', TagsViewSet, basename='tags')
+api_recipes_router_v1.register(r'ingredients', IngredientsViewSet,
                                basename='ingredients')
-api_recipes_router_v1.register(r'recipes', RecipesViewSet,
+api_recipes_router_v1.register(r'recipes', RecipeViewSet,
                                basename='recipes')
 
 urlpatterns = [
