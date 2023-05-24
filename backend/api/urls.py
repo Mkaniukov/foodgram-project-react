508546157c8720c1f1a0beb8d9ba7a2e637ca5eb
Murat <m.kaniukov@gmail.com>
2023-05-24 13:45:22 +0200

    on server2

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,4 +1,4 @@
-rom django.contrib import admin
+from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
