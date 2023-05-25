47d82c32fe9b1c2e0f0c677ea3053ad92ead4f96
Murat <m.kaniukov@gmail.com>
2023-05-25 21:00:34 +0200

    ok

--- backend/recipes/permissions.py
+++ backend/recipes/permissions.py
@@ -2,11 +2,19 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
 class IsOwnerOrReadOnly(BasePermission):
+    """
+    Разрешение на создание и изменение только для пользователя.
+    Остальным только чтение объекта.
+    """
     def has_object_permission(self, request, view, obj):
         return request.method in SAFE_METHODS or obj.author == request.user
 
 
 class IsAdminOrReadOnly(BasePermission):
+    """
+    Разрешение на создание и изменение только для админов.
+    Остальным только чтение объекта.
+    """
     def has_permission(self, request, view):
         return (request.method in SAFE_METHODS
                 or request.user and request.user.is_staff)
--- backend/users/permissions.py
+++ backend/users/permissions.py
@@ -2,6 +2,10 @@ from rest_framework import permissions
 
 
 class IsAuthorOnly(permissions.BasePermission):
+    """
+    Разрешение на изменение только для автора.
+    Остальным только чтение объекта.
+    """
     def has_permission(self, request, view):
         return (
             request.user.is_authenticated and request.method
